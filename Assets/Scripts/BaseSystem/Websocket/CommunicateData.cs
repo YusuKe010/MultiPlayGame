bc93206068bdb1bad251d2f2e498d3087d80d868
Yusuke17 <yuusuke.k1717@gmail.com>
2025-02-25 23:43:32 +0900

    同期機能

---
+++ Assets/Scripts/BaseSystem/Websocket/CommunicateData.cs
@@ -0,0 +1,12 @@
+﻿public class CommunicateData
+{
+    public string ID;
+    public string Command;
+    public string JsonBody;
+}
+
+public struct UserData
+{
+    public string ID;
+    public string Name;
+}
